634cbf8733916f1f5c765d8323c2d3f1341fe6af
Eubene Sa <eubene.sa@gmail.com>
2014-12-15 22:10:52 -0800

    Add mailer logic and view, set mailer delivery method to letter_opener for dev env

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,2 +1,8 @@
 class TestMailer < ApplicationMailer
+  default from: 'eubene.sa@gmail.com'
+
+  def send_email(name)
+    @name = name
+    mail(to: 'to@example.com', subject: 'Test email')
+  end
 end
